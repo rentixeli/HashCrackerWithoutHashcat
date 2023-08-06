f45f0ae9c7c880c45f02958fa6b1d18c7dd5d270
rentixeli <73320873+rentixeli@users.noreply.github.com>
2023-08-06 20:57:52 +0300

    Update py.py

--- py.py
+++ py.py
@@ -1,15 +1,26 @@
 import hashlib
 
-key = 'VP10' # 3 missing
-hash= 'de2a4ec338f3d03fe9a8dc44db61d756a1bf7906'
+# VP1000b: de2a4ec338f3d03fe9a8dc44db61d756a1bf7906
+
+key = input('Provide part of the known password only: ')
+hash_value = input('Provide the password\'s hash: ')
+totalN = input('Provide the full masked password: ')
 abc = 'qazxswedcvfrtgbnhyujmkiolpPOLIKMJUYHNBGTRFVCDEWSXZAQ!@#$%^&*()-+1234567890'
 
-for i in abc:
-	for j in abc:
-		for l in abc:
-			newstr = key + i + j + l
-			encode = newstr.encode()
-			hashed = hashlib.sha1(encode)
-			hexa = hashed.hexdigest()
-			if hash == hexa:
-				print (newstr + ' : ' + hexa)
+def cal(newstr):
+    global hash_value, hexa
+
+    for letter in abc:
+        current_str = newstr + letter
+
+        if len(current_str) < len(totalN):
+            cal(current_str)
+        else:
+            encode = current_str.encode()
+            hashed = hashlib.sha1(encode)
+            hexa = hashed.hexdigest()
+            if hash_value == hexa:
+                print('\n\rThe clear-text password: '+current_str + '\n\rThe hash: ' + hexa)
+                return
+
+cal(key)
